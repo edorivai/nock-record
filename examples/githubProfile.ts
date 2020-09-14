3d31b8e0ef9a513692ec29a726d16b518ecf27c9
Natacha Tomkinson <natacha.tomkinson@york.ac.uk>
2020-09-14 17:20:03 +0100

    ISSUE-24: Update examples
    Github's api requires setting a user-agent as per https://developer.github.com/v3/#user-agent-required

--- examples/githubProfile.ts
+++ examples/githubProfile.ts
@@ -1,8 +1,12 @@
 import * as request from 'superagent';
 
 export async function getGithubProfile(username: string) {
-	const response = await request.get(`https://api.github.com/users/${username}`);
+    try {
+        const response = await request
+            .get(`https://api.github.com/users/${username}`)
+            .set('User-Agent', 'edorivai/nock-record example');
 
-	const { url, name, location } = response.body;
-	return { url, name, location };
+        const { url, name, location } = response.body;
+        return { url, name, location };
+    } catch {}
 }
