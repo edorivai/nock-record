db8e3d84f43d28456ee2f06cf702618b7ebd99c9
Natacha Tomkinson <natacha.tomkinson@york.ac.uk>
2020-09-16 13:48:55 +0100

    ISSUE-24: Codereview – remotes try/catch clause

--- examples/githubProfile.ts
+++ examples/githubProfile.ts
@@ -1,12 +1,10 @@
-import * as request from 'superagent';
+import * as request from "superagent";
 
 export async function getGithubProfile(username: string) {
-    try {
-        const response = await request
-            .get(`https://api.github.com/users/${username}`)
-            .set('User-Agent', 'edorivai/nock-record example');
+  const response = await request
+    .get(`https://api.github.com/users/${username}`)
+    .set("User-Agent", "edorivai/nock-record example");
 
-        const { url, name, location } = response.body;
-        return { url, name, location };
-    } catch {}
+  const { url, name, location } = response.body;
+  return { url, name, location };
 }
